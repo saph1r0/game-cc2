d958c80e91c58da189506e716dc0a6321ac38ee3
saph1r0 <sophiqwe123@gmail.com>
2024-06-30 20:36:02 -0500

    se prueba build y ejecutable

---
+++ build/CMakeFiles/juego.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/juego.dir/src/Fireball.cpp.o"
+  "CMakeFiles/juego.dir/src/Fireball.cpp.o.d"
+  "CMakeFiles/juego.dir/src/Platform.cpp.o"
+  "CMakeFiles/juego.dir/src/Platform.cpp.o.d"
+  "CMakeFiles/juego.dir/src/Player.cpp.o"
+  "CMakeFiles/juego.dir/src/Player.cpp.o.d"
+  "CMakeFiles/juego.dir/src/main.cpp.o"
+  "CMakeFiles/juego.dir/src/main.cpp.o.d"
+  "juego"
+  "juego.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/juego.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
